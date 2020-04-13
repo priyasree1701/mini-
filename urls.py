9fd183c05b0fa3695a03fb39f72534ee36c178bd
deekshalakumarapu <59387321+deekshalakumarapu@users.noreply.github.com>
2020-04-13 23:02:01 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,15 +1,22 @@
-from django.urls import path
-from . import views
+"""dp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
 urlpatterns = [
-    path('', views.hi,name='unused medicine'),
-    path('hi.html', views.hi),
-    path('signup.html', views.signUp),
-    path('signin.html', views.signin),
-    path('contact.html', views.contact),
-    path('registerationSuccess', views.registrationSuccess),
-    path('home.html', views.home),
-    path('loginvalidation', views.loginsucess),
-
-    path('medilist', views.medilist),
-
-    ]
+    path('admin/', admin.site.urls),
+    path('',include('demoapp.urls'))
+]
